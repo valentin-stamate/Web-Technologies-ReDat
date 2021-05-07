8c33833a8e7c9d96c2c8dd93997d0b2de7cea944
Laurentiu <mihai.laurentiu24@yahoo.com>
2021-05-07 21:17:30 +0300

    added Post class and updated gitignore

--- external_api/reddit_data.py
+++ external_api/reddit_data.py
@@ -1,47 +1,73 @@
-import requests
 import pandas
+import requests
 
-CLIENT_ID = 'B1hmLU6wbT6SdQ'
-SECRET_KEY = 'lZh7FRhuCZSTkPOIqHIvp-fkrHqKew'
+from secret_var.secrets import *
+
 
 # note that CLIENT_ID refers to 'personal use script' and SECRET_TOKEN to 'token'
-auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
+def get_hot_posts(topic, limit):
+    auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
 
-with open('../pw.txt', 'r') as f:
-    passwd = f.read()
-# here we pass our login method (password), username, and password
-data = {'grant_type': 'password',
-        'username': '-lorenzo112-',
-        'password': passwd}
+    # here we pass our login method (password), username, and password
+    data = {'grant_type': 'password',
+            'username': REDDIT_USERNAME,
+            'password': REDDIT_PASSWORD}
 
-# setup our header info, which gives reddit a brief description of our app
-headers = {'User-Agent': 'ReDatAPI v0.0.1'}
+    # setup our header info, which gives reddit a brief description of our app
+    headers = {'User-Agent': 'ReDatAPI v0.0.1'}
 
-# send our request for an OAuth token
-res = requests.post('https://www.reddit.com/api/v1/access_token',
-                    auth=auth, data=data, headers=headers)
+    # send our request for an OAuth token
+    res = requests.post('https://www.reddit.com/api/v1/access_token',
+                        auth=auth, data=data, headers=headers)
 
-# convert response to JSON and pull access_token value
-TOKEN = res.json()['access_token']
+    # convert response to JSON and pull access_token value
+    token = res.json()['access_token']
 
-# add authorization to our headers dictionary
-headers['Authorization'] = f"bearer {TOKEN}"
+    # add authorization to our headers dictionary
+    headers['Authorization'] = f"bearer {token}"
 
-# while the token is valid (~2 hours) we just add headers=headers to our requests
-requests.get('https://oauth.reddit.com/api/v1/me', headers=headers)
+    # while the token is valid (~2 hours) we just add headers=headers to our requests
+    requests.get('https://oauth.reddit.com/api/v1/me', headers=headers)
 
-res = requests.get('https://oauth.reddit.com/r/games/hot', headers=headers, params={'limit': '100'})
+    res = requests.get('https://oauth.reddit.com/r/{topic}/hot'.format(topic=topic), headers=headers,
+                       params={'limit': '{limit}'.format(limit=limit)})
 
-data_frame = pandas.DataFrame()
+    data_frame = pandas.DataFrame()
+    posts = [] * 100
+    for post in res.json()['data']['children']:
+        posts.append(post)
 
-for post in res.json()['data']['children']:
-    data_frame = data_frame.append({
-        'subreddit': post['data']['subreddit'],
-        'title': post['data']['title'],
-        'selftext': post['data']['selftext'],
-        'author': post['data']['author'],
-        'score': post['data']['score']
-    }, ignore_index=True)
+    # print(post['data'].keys())  # items that can be obtained
+    return posts
 
-# print(post['data'].keys()) #items that can be obtained
-print(data_frame)
+
+def get_trending_subreddits(limit):
+    auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
+
+    # here we pass our login method (password), username, and password
+    data = {'grant_type': 'password',
+            'username': REDDIT_USERNAME,
+            'password': REDDIT_PASSWORD}
+
+    # setup our header info, which gives reddit a brief description of our app
+    headers = {'User-Agent': 'ReDatAPI v0.0.1'}
+
+    # send our request for an OAuth token
+    res = requests.post('https://www.reddit.com/api/v1/access_token',
+                        auth=auth, data=data, headers=headers)
+
+    # convert response to JSON and pull access_token value
+    token = res.json()['access_token']
+
+    # add authorization to our headers dictionary
+    headers['Authorization'] = f"bearer {token}"
+
+    # while the token is valid (~2 hours) we just add headers=headers to our requests
+    requests.get('https://oauth.reddit.com/api/v1/me', headers=headers)
+
+    res = requests.get('https://oauth.reddit.com/r/trending_subreddits', headers=headers,
+                       params={'limit': '{limit}'.format(limit=limit)})
+    posts = [] * 100
+    for post in res.json()['data']['children']:
+        posts.append(post)
+    return posts
--- init.py
+++ init.py
@@ -1,5 +1,9 @@
+import pandas
+
 from database.connection.connection import execute_sql
 from database.models.user_model import User
+from external_api.reddit_data import get_hot_posts, get_trending_subreddits
+from external_api.reddit_post import Post
 
 execute_sql('''CREATE TABLE IF NOT EXISTS users (
     id SERIAL,
@@ -13,21 +17,48 @@ execute_sql('''CREATE TABLE IF NOT EXISTS users (
 )''')
 print("Table users created successfully")
 
-user = User(username='ValentinSt', firstname='Valentin', lastname='Stamate', email='stamtevalentin125@gmail.com', password='123456789')
+user = User(username='ValentinSt', firstname='Valentin', lastname='Stamate', email='stamtevalentin125@gmail.com',
+            password='123456789')
 user.save()
 
-user = User(username='Lorenzo', firstname='Iphone', lastname='Laurentiu', email='iphonelaurentiu@gmail.com', password='123456789')
+user = User(username='Lorenzo', firstname='Iphone', lastname='Laurentiu', email='iphonelaurentiu@gmail.com',
+            password='123456789')
 user.save()
 
 user = User(username='ValentinSt', password='123456789')
 if user.login():
     print(user)
-else :
+else:
     print("Login failed")
 
 user = User(username='Lex', password='123456789')
 print(user.is_valid())
 
-
-
-
+posts_list = get_hot_posts('games',10)
+data_frame = pandas.DataFrame()
+my_post = None
+for post in posts_list:
+    my_post = Post(post)
+    data_frame = data_frame.append({
+        'subreddit': post['data']['subreddit'],
+        'title': post['data']['title'],
+        'selftext': post['data']['selftext'],
+        'author': post['data']['author'],
+        'score': post['data']['score']
+    }, ignore_index=True)
+print(data_frame)
+print(my_post.title)
+trending = get_trending_subreddits(10)
+data_frame = pandas.DataFrame()
+my_post = None
+for post in trending:
+    my_post = Post(post)
+    data_frame = data_frame.append({
+        'subreddit': post['data']['subreddit'],
+        'title': post['data']['title'],
+        'selftext': post['data']['selftext'],
+        'author': post['data']['author'],
+        'score': post['data']['score']
+    }, ignore_index=True)
+print(data_frame)
+# print(post['data'].keys()) #items that can be obtained
