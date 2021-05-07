659fdd9ef89b9f5310e18ae40023544890d75959
Laurentiu <mihai.laurentiu24@yahoo.com>
2021-05-07 17:01:42 +0300

    updated encryption algorithm to sha256

--- database/util/password_encryption.py
+++ database/util/password_encryption.py
@@ -5,9 +5,8 @@ class PasswordEncryption:
 
     @staticmethod
     def encrypt_password(password, date_created):
-        # salt = os.urandom(32)  # A random salt for this user
-        salt = abs(hash(date_created)).to_bytes(16, 'big')
+        password += str(date_created)
+        return str(hashlib.sha256(password.encode()).hexdigest())
 
-        encoded_password = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
 
-        return str(encoded_password)[2:len(encoded_password) - 2]
+print(PasswordEncryption.encrypt_password('my pass', 'data'))
