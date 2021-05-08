cc007f63830713983ab85ab217a13b835ca9d961
ValentinSt <stamatevalentin125@gmail.com>
2021-05-08 13:29:44 +0300

    expliciting errors

--- database/models/user_model.py
+++ database/models/user_model.py
@@ -29,39 +29,28 @@ class User:
             print(f"User {self.username} saved")
         except Exception as e:
             print(e)
-            return False
+            return {'status': False, 'message': e}
 
         self.get_user_id()
 
-        return True
+        return {'status': True, 'message': 'User created successfully'}
 
     def update(self):
         if self.user_id == 0:
-            print("User should be saved or logged first")
-            return False
+            return {'status': False, 'message': 'User should be logged first'}
 
         execute_sql(
             f"UPDATE users SET username = '{self.username}', firstname = '{self.firstname}', lastname = '{self.lastname}', password = '{self.password}' WHERE id = {self.user_id}")
 
-        return True
-
-    @staticmethod
-    def __get_user_by_key(key, value):
-        try:
-            row = execute_sql(f"SELECT * FROM users WHERE {key} = {value}")[0]
-            return User(user_id=row[0], username=row[1], firstname=row[2], lastname=row[3],
-                        email=row[4], password=row[5], date_created=row[6])
-        except IndexError:
-            print(f"User with key:{key} and value:{value} not found")
-            return None
+        return {'status': True, 'message': 'User updated successfully'}
 
     # AUTHENTICATION
     def login(self):
 
-        db_user = User.get_by_username(self.username)
+        db_user = User.get_by_username(self.username)['object']
 
         if db_user is None:
-            return False
+            return {'status': False, 'message': "User doesn't exist"}
 
         if db_user.password == PasswordEncryption.encrypt_password(self.password, db_user.date_created):
             self.user_id = db_user.user_id
@@ -71,30 +60,30 @@ class User:
             self.email = db_user.email
             self.password = ''
             self.date_created = db_user.date_created
-            return True
+            return {'status': True, 'message': 'Login success'}
 
-        return False
+        return {'status': True, 'message': 'Login failed'}
 
     # VALIDATION
     def is_valid(self):
         if len(self.username) < 3:
-            return False
+            return {'status': False, 'message': 'Invalid username'}
 
         if len(self.firstname) < 8:
-            return False
+            return {'status': False, 'message': 'Invalid firstname'}
 
         if len(self.lastname) < 8:
-            return False
+            return {'status': False, 'message': 'Invalid lastname'}
 
         email_regex = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'
 
         if not re.search(email_regex, self.email):
-            return False
+            return {'status': False, 'message': 'Invalid email'}
 
         if len(self.password) < 8:
-            return False
+            return {'status': False, 'message': 'Invalid password'}
 
-        return True
+        return {'status': True, 'message': 'Invalid email'}
 
     # GETTERS
     @staticmethod
@@ -106,7 +95,17 @@ class User:
         return User.__get_user_by_key("id", f"{user_id}")
 
     def get_user_id(self):
-        self.user_id = User.__get_user_by_key('username', f"'{self.username}'").user_id
+        self.user_id = User.__get_user_by_key('username', f"'{self.username}'")['object'].user_id
+
+    @staticmethod
+    def __get_user_by_key(key, value):
+        try:
+            row = execute_sql(f"SELECT * FROM users WHERE {key} = {value}")[0]
+            user = User(user_id=row[0], username=row[1], firstname=row[2], lastname=row[3],
+                        email=row[4], password=row[5], date_created=row[6])
+            return {'object': user, 'message': 'Success'}
+        except IndexError:
+            return {'object': None, 'message': f"User with key:{key} and value:{value} not found"}
 
     def __str__(self):
         return f"User {self.user_id, self.username, self.firstname, self.lastname, self.email, self.password, self.date_created}"
