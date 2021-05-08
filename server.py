ec2012335d74e51b25731c425131c87798825510
ValentinSt <stamatevalentin125@gmail.com>
2021-05-08 14:57:08 +0300

    static files rendering

--- server.py
+++ server.py
@@ -1,4 +1,11 @@
+import os
 
+content_type = {'.html': 'text/html',
+                '.css': 'text/css'
+                }
+
+
+# TEMPLATE RENDERING WITH CONTEXT
 def render_template(template_name='index.html', context={}):
     template_name = f"templates/{template_name}"
     html_string = ""
@@ -10,6 +17,18 @@ def render_template(template_name='index.html', context={}):
     return html_string
 
 
+# RENDERING FILES SUCH AS IMAGES, CSS, ETC
+def render_file(file_name):
+    file_name = file_name[1:len(file_name)]
+    file_string = ""
+
+    with open(file_name, 'r') as f:
+        file_string = f.read()
+
+    return file_string
+
+
+# CONTROLLERS
 def home(environ):
     return render_template(template_name='index.html', context={"data": "ana are mere"})
 
@@ -18,13 +37,20 @@ def page_not_found(environ):
     return render_template(template_name='404.html')
 
 
+# CONTROLLER HANDLER
 def app(environ, start_response):
     path = environ.get("PATH_INFO")
     if path.endswith("/"):
         path = path[:-1]
 
+    filename, file_extension = os.path.splitext(path)
+
+    print(f"Requested file: {path}")
+
     if path == "":
         data = home(environ)
+    elif path.startswith('/static'):
+        data = render_file(path)
     else:
         data = page_not_found(environ)
 
@@ -32,12 +58,13 @@ def app(environ, start_response):
 
     start_response(
         f"200 OK", [
-            ("Content-Type", "text/html"),
+            ("Content-Type", content_type.get(file_extension, 'text/html')),
             ("Content-Length", str(len(data)))
         ]
     )
 
     return iter([data])
+
 
 # pipenv shell
 # gunicorn server:app --reload
