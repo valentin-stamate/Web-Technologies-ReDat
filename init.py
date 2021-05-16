c9ff6d35a31c31f28b10a68f68baac1f21764790
ValentinSt <stamatevalentin125@gmail.com>
2021-05-16 11:39:03 +0000

    adding image url to user

--- init.py
+++ init.py
@@ -1,9 +1,7 @@
-import pandas
-
 from services.server.database.connection.connection import execute_sql
 from services.server.database.models.user_model import UserModel
-from services.external.reddit_api.reddit_data import get_hot_posts, get_trending_subreddits
-from services.external.reddit_api.reddit_post import Post
+
+execute_sql('''DROP TABLE IF EXISTS users''')
 
 execute_sql('''CREATE TABLE IF NOT EXISTS users (
     id SERIAL,
@@ -12,6 +10,7 @@ execute_sql('''CREATE TABLE IF NOT EXISTS users (
     lastname VARCHAR (255) NOT NULL,
     email VARCHAR (255) NOT NULL UNIQUE ,
     password VARCHAR (255) NOT NULL,
+    image_url VARCHAR(255) NOT NULL,
     date_created TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
 )''')
@@ -24,41 +23,3 @@ user.save()
 user = UserModel(username='Lorenzo', firstname='Iphone', lastname='Laurentiu', email='iphonelaurentiu@gmail.com',
                  password='123456789')
 user.save()
-
-user = UserModel(username='ValentinSt', password='123456789')
-if user.login():
-    print(user)
-else:
-    print("Login failed")
-
-user = UserModel(username='Lex', password='123456789')
-print(user.is_valid())
-
-posts_list = get_hot_posts('games',10)
-data_frame = pandas.DataFrame()
-my_post = None
-for post in posts_list:
-    my_post = Post(post)
-    data_frame = data_frame.append({
-        'subreddit': post['data']['subreddit'],
-        'title': post['data']['title'],
-        'selftext': post['data']['selftext'],
-        'author': post['data']['author'],
-        'score': post['data']['score']
-    }, ignore_index=True)
-print(data_frame)
-print(my_post.title)
-trending = get_trending_subreddits(10)
-data_frame = pandas.DataFrame()
-my_post = None
-for post in trending:
-    my_post = Post(post)
-    data_frame = data_frame.append({
-        'subreddit': post['data']['subreddit'],
-        'title': post['data']['title'],
-        'selftext': post['data']['selftext'],
-        'author': post['data']['author'],
-        'score': post['data']['score']
-    }, ignore_index=True)
-print(data_frame)
-# print(post['data'].keys()) #items that can be obtained
--- util/instance/user_data.py
+++
@@ -1,7 +0,0 @@
-from util.instance.user import User
-
-
-class UserData:
-    def __init__(self, user: User, topics=[]):
-        self.user = user
-        self.topics = topics
