fc8be195b1d088d99f0f99922907a86925a38dcb
Mihai Laurentiu <mihai.laurentiu24@yahoo.com>
2021-05-20 15:16:41 +0300

    added upvote ratio statistic

--- services/external/reddit_api/reddit_data.py
+++ services/external/reddit_api/reddit_data.py
@@ -41,40 +41,6 @@ def get_hot_posts(topic, limit=25):
     return posts
 
 
-def get_hot_posts_ups(topic, limit=25):
-    auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
-    topic = topic.lower()
-    # here we pass our login method (password), username, and password
-    data = {'grant_type': 'password',
-            'username': REDDIT_USERNAME,
-            'password': REDDIT_PASSWORD}
-
-    # setup our header info, which gives reddit_api a brief description of our app
-    headers = {'User-Agent': 'ReDatAPI v0.0.1'}
-
-    # send our request for an OAuth token
-    res = requests.post('https://www.reddit.com/api/v1/access_token',
-                        auth=auth, data=data, headers=headers)
-
-    # convert response to JSON and pull access_token value
-    token = res.json()['access_token']
-
-    # add authorization to our headers dictionary
-    headers['Authorization'] = f"bearer {token}"
-
-    # while the token is valid (~2 hours) we just add headers=headers to our requests
-    requests.get('https://oauth.reddit.com/api/v1/me', headers=headers)
-
-    res = requests.get('https://oauth.reddit.com/r/{topic}/hot'.format(topic=topic), headers=headers,
-                       params={'limit': '{limit}'.format(limit=limit)})
-
-    posts = [] * limit
-    for post in res.json()['data']['children']:
-        posts.append(post)
-    # print(post['data'].keys())  # items that can be obtained
-    return posts
-
-
 def get_trending_subreddits(limit=25, before=None):
     auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
 
--- services/external/statistics/statistics.py
+++ services/external/statistics/statistics.py
@@ -36,7 +36,7 @@ def general_up_votes_statistic():
         plt.title("Most up voted 10 subreddits")
         plt.ylabel("Up Votes")
         plt.xlabel("Subreddits")
-        plt.savefig("static/stats/ups_stats.svg")
+        plt.savefig("static/stats/general_ups_stats.svg")
         plt.show()
         print("(General up votes statistic) Last updated : " + str(current_timestamp()))
         time.sleep(3600)
@@ -53,25 +53,48 @@ def subreddit_comment_stats():
     comment_stats = [[0] * 6 for i in range(len(topics))]
     time_ax = np.arange(0, 60, 10)
     while True:
-        current_index = 0
         for topic in topics:
             try:
-                res = get_hot_posts(topic=topic, limit=10)
-                print(topics.index(topic))
+                res = get_hot_posts(topic=topic, limit=100)
                 for post in res:
                     comment_stats[topics.index(topic)][5] += post['data']['num_comments']
             except Exception:
                 print(topic)
-            print(comment_stats[topics.index(topic)])
             plt.figure(figsize=(15, 8))
             fig, ax = plt.subplots()
             ax.plot(time_ax, comment_stats[topics.index(topic)])
             ax.set(xlabel='time (m)', ylabel='nr comments',
                    title='Comments in the last hour in {topic}'.format(topic=topic))
             ax.grid()
-            fig.savefig("static/stats/{topic}_stats.svg".format(topic=topic))
+            fig.savefig("static/stats/comments/{topic}_stats.svg".format(topic=topic))
             plt.close('all')
             print("({topic} comments statistic) Last updated : ".format(topic=topic) + str(current_timestamp()))
 
         matrix_shift(comment_stats)
+        time.sleep(600)
+
+
+def upvote_statistic():
+    upvote_stats = [[0] * 6 for i in range(len(topics))]
+    time_ax = np.arange(0, 60, 10)
+    while True:
+        for topic in topics:
+            try:
+                res = get_hot_posts(topic=topic, limit=100)
+                for post in res:
+                    upvote_stats[topics.index(topic)][5] += post['data']['upvote_ratio']
+                upvote_stats[topics.index(topic)][5] /= len(res)
+            except Exception:
+                print(topic)
+            plt.figure(figsize=(15, 8))
+            fig, ax = plt.subplots()
+            ax.stackplot(time_ax, upvote_stats[topics.index(topic)])
+            ax.set(xlabel='time (m)', ylabel='nr comments',
+                   title='Comments in the last hour in {topic}'.format(topic=topic))
+            ax.grid()
+            fig.savefig("static/stats/upvotes/{topic}_upvote_stats.svg".format(topic=topic))
+            plt.close('all')
+            print("({topic} upvote ratio statistic) Last updated : ".format(topic=topic) + str(current_timestamp()))
+
+        matrix_shift(upvote_stats)
         time.sleep(600)
