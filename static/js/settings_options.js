1dd6f7fa1710ca95df30c2f4dcd164d1d2b73785
ValentinSt <stamatevalentin125@gmail.com>
2021-05-25 15:00:48 +0300

    user data message and bug fixes

--- static/js/settings_options.js
+++ static/js/settings_options.js
@@ -1,25 +1,41 @@
 import {refreshTopics} from "./topics.js";
+import {initListeners} from "./user_profile.js";
 
 function setCurrentMenu(menuName) {
     localStorage.setItem('menu', menuName);
 }
 
 let currentSettingMenu = document.getElementById("current-settings-menu");
+
 let generalOption = document.getElementById("general");
 let appearanceOption = document.getElementById("appearance");
 let topicsOption = document.getElementById("topics");
+
+const generalOptionsElement = document.getElementById("general-option");
+const generalOptionsHTML = generalOptionsElement.innerHTML;
+generalOptionsElement.innerHTML = '';
+
+const appearanceOptionsElement = document.getElementById("appearance-option");
+const appearanceOptionsHTML = appearanceOptionsElement.innerHTML;
+appearanceOptionsElement.innerHTML = '';
+
+const topicOptionsElement = document.getElementById("topics-option");
+const topicOptionsHTML = topicOptionsElement.innerHTML;
+topicOptionsElement.innerHTML = '';
+
 generalOption.addEventListener("click", general);
 appearanceOption.addEventListener("click", appearance);
 topicsOption.addEventListener("click", topics);
 
 function general() {
     setCurrentMenu("general-option");
-    currentSettingMenu.innerHTML = document.getElementById("general-option").innerHTML;
+    currentSettingMenu.innerHTML = generalOptionsHTML;
+    initListeners()
 }
 
 function appearance() {
     setCurrentMenu("appearance-option");
-    currentSettingMenu.innerHTML = document.getElementById("appearance-option").innerHTML;
+    currentSettingMenu.innerHTML = appearanceOptionsHTML;
 
     toggleThemeButton = document.getElementById("btn-toggle-theme");
     toggleThemeButton.addEventListener('click', function () {
@@ -29,7 +45,7 @@ function appearance() {
 
 function topics() {
     setCurrentMenu("topics-option");
-    currentSettingMenu.innerHTML = document.getElementById("topics-option").innerHTML;
+    currentSettingMenu.innerHTML = topicOptionsHTML;
     refreshTopics();
 }
 
--- static/js/user_profile.js
+++ static/js/user_profile.js
@@ -3,19 +3,30 @@ import {UPDATE_USER_ENDPOINT} from "./endpoints.js";
 import {sendFetchRequest} from "./request/request_handler.js";
 import {getCookie, USER_AUTH_COOKIE} from "./util/cookie.js";
 
-const userDataForm = document.getElementById('user-data-form');
-const saveButton = document.getElementById('save-button');
-saveButton.addEventListener('click', function(e) {
-    e.preventDefault();
-    const userData = getFormData(userDataForm);
+export function initListeners() {
+    const userDataForm = document.getElementById('user-data-form');
+    const saveButton = document.getElementById('save-button');
 
-    sendFetchRequest(UPDATE_USER_ENDPOINT, 'POST', {'token': getCookie(USER_AUTH_COOKIE), ...userData})
-        .then(response => {
-            return response.text();
-        })
-        .then(data => {
-            window.location = '/profile';
-        });
-    /* TODO error checking */
+    saveButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        const userData = getFormData(userDataForm);
 
-});
+        sendFetchRequest(UPDATE_USER_ENDPOINT, 'POST', {'token': getCookie(USER_AUTH_COOKIE), ...userData})
+            .then(response => {
+                const errorTextElement = document.getElementById('error-text-user-data');
+                errorTextElement.innerHTML = '';
+
+                if (response.status === 200) {
+                    console.log("Success")
+                    window.location = '/profile';
+                } else {
+                    response.json().then(data => {
+                        errorTextElement.innerHTML = data.message;
+                    })
+                }
+            });
+    });
+}
+
+initListeners();
+
